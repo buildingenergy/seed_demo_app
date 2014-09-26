83e56732ef6441ce0696bf163efafda29ae4f462
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2014-09-25 20:06:11 -0700

    more stats

--- stats/views.py
+++ stats/views.py
@@ -6,19 +6,20 @@
 from django.contrib.auth.decorators import login_required
 from annoying.decorators import render_to
 from seed.models import BuildingSnapshot
-from django.db.models import Avg
+from django.db.models import Avg, Min, Max, StdDev
 
 
 @login_required
 @render_to('stats/stats.html')
 def stats(request):
-    """get a couple averarages to display"""
+    """get a couple averages to display"""
     stats_dict = BuildingSnapshot.objects.filter(
         canonical_building__active=True
-    ).aggregate(Avg('gross_floor_area'))
-    stats_dict.update(
-        BuildingSnapshot.objects.filter(
-            canonical_building__active=True
-        ).aggregate(Avg('year_built'))
+    ).aggregate(
+        Avg('gross_floor_area'),
+        Min('gross_floor_area'),
+        Max('gross_floor_area'),
+        StdDev('gross_floor_area'),
     )
+
     return locals()
